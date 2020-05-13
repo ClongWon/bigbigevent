29201c07791f8fc25f5945f969a3f2ff63d1aedb
ClongWon <736000894@163.com>
2020-05-13 19:47:01 +0800

    注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,28 @@
+$(function() {
+    // 切换登录和注册的盒子
+    $('.goto-register a').click(function() {
+        $('#register').show().prev().hide();
+    });
+    $('.goto-login a').click(function() {
+            $('#register').hide().prev().show();
+        })
+        // 监听注册表单的提交事件
+    $('#register form').on('submit', function(e) {
+        // 阻止页面跳转
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax({
+            type: 'post',
+            url: 'http://www.liulongbin.top:3007/api/reguser',
+            data: data,
+            success: function(res) {
+                alert(res.message);
+                // 注册成功的话，隐藏注册盒子，并显示登录盒子
+                if (res.status === 0) {
+                    $('#register').hide().prev().show();
+                }
+            }
+        })
+
+    })
+});
